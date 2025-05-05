3555442ae86e6e57914cb7c4c2763a0e89609595
tedym <tedym@MIRLO>
2025-05-05 00:13:28 -0400

    Agregar archivos de proyecto.

---
+++ Login/MenuPrincipal.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LoginApp
+{
+    public partial class MenuPrincipal : Window
+    {
+        public MenuPrincipal()
+        {
+            InitializeComponent();
+        }
+    }
+}
