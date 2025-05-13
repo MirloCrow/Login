eb8f9761399da555be7057990f4fcde032d69bae
Roberto Esparza <roberto.esparza.p@protonmail.com>
2025-05-12 21:51:24 -0400

    Implementación directorio MVVM, conexión a BD local con SQL Express y readme actualizado

---
+++ Login/Data/ConexionLocal.cs
@@ -0,0 +1,14 @@
+﻿namespace Login.Data
+{
+    public static class ConexionLocal
+    {
+        // En Servidor se debe poner la dirección local según el dispositivo
+        public static string Servidor = "R5\\SQLEXPRESS";
+        public static string BaseDatos = "SistemaFERCO";
+
+        public static string ObtenerCadenaConexion()
+        {
+            return $"Server={Servidor};Database={BaseDatos};Trusted_Connection=True;Encrypt=False;";
+        }
+    }
+}
---
+++ Login/Login.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using Login.Data;
+using Login.Model;
+
+namespace LoginApp
+{
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string usuario = txtUsuario.Text;
+            string password = txtPassword.Password;
+
+            var usuarioValido = UsuarioDAO.ValidarLogin(usuario, password);
+
+            if (usuarioValido != null && usuarioValido.Rol == "admin")
+            {
+                var menu = new MenuPrincipal(); // asegúrate de tener esta ventana
+                menu.Show();
+                this.Close();
+            }
+            else
+            {
+                lblMensaje.Text = "Credenciales incorrectas.";
+            }
+        }
+    }
+}
